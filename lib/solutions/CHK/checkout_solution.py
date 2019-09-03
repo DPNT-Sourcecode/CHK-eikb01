bcb485fbcd8eb90a30dbab160096b23512567b8b
sbx_user1051 <sbx_user1051@169.254.129.253>
2019-09-03 18:03:19 +0000

    Tue Sep 03 18:03:19 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,10 +9,22 @@ def checkout(skus):
                 'C': [20, None],
                 'D': [15, None]
             }
-    qty_matrix = get_qty_matrix()
+    qty_matrix = get_qty_matrix(skus)
     for sku, qty in qty_matrix.items():
-        
-
+        if sku in basedata:
+            if isinstance(basedata[sku][1], tuple):
+                # check if special offer is present
+                full_bundles = qty // basedata[sku][1][0]
+                left_over = qty % basedata[sku][1][0]
+                sku_cost = (full_bundles * basedata[sku][1][1]) + (left_over * basedata[sku][0])
+                total_value += sku_cost
+            else:
+                # no bundle offers are present
+                sku_cost = qty * basedata[sku][0]
+                total_value += sku_cost
+        else:
+            # sku price is not declared beforehand, hence can't compute value
+            return -1
 
     return total_value
 
@@ -32,3 +44,6 @@ def get_qty_matrix(skus):
 
     print(qty_matrix)
     return qty_matrix
+
+
+print(checkout("ABAABCA"))
